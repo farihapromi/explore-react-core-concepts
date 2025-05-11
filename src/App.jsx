d4fa17b6f16f0950b5572f657016bb37d32a958e
Fariha Promi <farihapromi2611@gmail.com>
2025-05-11 11:52:21 +0600

    Send data destrucring props

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,43 @@ function App() {
 
   return (
     <>
-    <Developer></Developer>
+    <Person></Person>
+    <Developer name="Shuvo" salary="60000"></Developer>
+    <Device name="Laptop" price="12"></Device>
+    <Device name="Mobile" price="10"></Device>
+
+    <Device name="Watch" price="7"></Device>
+
+   
+
       
     </>
   )
 }
-function Developer(){
+function Device(props){
+  return(
+    <div>
+      <h3>This device is : {props.name}</h3>
+      <h3>Price is :{props.price}</h3>
+    </div>
+  )
+}
+function Person(){
+  const person1={
+    name:"anabia",
+    age:3
+  }
+  return(
+    <div>
+      <h3>Name is: {person1.name}</h3>
+      <h3>Age is : {person1.age}</h3>
+
+    </div>
+  )
+}
+function Developer({name,salary}){
+  //destrucring
+
   const developStyle={
     margin:"20px",
     padding:"10px",
@@ -22,7 +53,9 @@ function Developer(){
   }
   return(
     <div style={developStyle}>
-      <h3>This is a Developer</h3>
+     
+      <h3>{name}</h3>
+      <h3>{salary}</h3>
     </div>
   )
 }
